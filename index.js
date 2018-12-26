608f7a6ca72cf487dba9a8c04eb6834afbff735b
visa4 <antonino.visalli@gmail.com>
2018-12-26 22:47:18 +0100

    Fix library

--- index.js
+++ index.js
@@ -8,6 +8,9 @@
 module.exports = {
     Application : require('./lib/core/Application.js'),
     Archive : require('./lib/archive/Archive.js'),
+    core : {
+        PaperToastNotification : require('./lib/core/PaperToastNotification.js'),
+    },
     EvtManager : require('./lib/event/EvtManager.js'),
     hydrator : {
         HydratorPluginManager : require('./lib/hydrator/HydratorPluginManager.js'),
@@ -17,13 +20,31 @@ module.exports = {
         PropertyHydrator : require('./lib/hydrator/PropertyHydrator.js'),
         strategy : {
             HydratorStrategy : require('./lib/hydrator/strategy/HydratorStrategy.js'),
-            NumberStrategy : require('./lib/hydrator/strategy/NumberStrategy.js'),
+            NumberStrategy : require('./lib/hydrator/strategy/NumberStrategy.js')
         }
     },
     ModuleConfig : require('./lib/core/ModuleConfig.js'),
     ServiceManager : require('./lib/service-manager/ServiceManager.js'),
+    sport : {
+        model : {
+            Match : require('./lib/sport/model/Match.js'),
+            Player : require('./lib/sport/model/Player.js'),
+            Staff : require('./lib/sport/model/Staff.js'),
+            Team : require('./lib/sport/model/Team.js')
+        }
+    },
     storage : {
-        StoragePluginManager : require('./lib/storage/StoragePluginManager.js')
-    }
+        StoragePluginManager : require('./lib/storage/StoragePluginManager.js'),
+        Storage : require('./lib/storage/Storage.js'),
+        adapter : {
+            DexieCollection : require('./lib/storage/indexed-db/dexie/DexieCollection.js'),
+            DexieManager : require('./lib/storage/indexed-db/dexie/DexieManager.js'),
+            LocalStorage : require('./lib/storage/local-storage/LocalStorage.js'),
+        },
+        model : {
+            Dates : require('./lib/storage/model/Dates'),
+        }
+    },
+    Utils : require('./lib/Utils.js')
 };
 
