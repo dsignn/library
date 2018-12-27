e0778aaf598c26ce236d7a475442eec17a36c574
visa4 <antonino.visalli@gmail.com>
2018-12-27 18:31:34 +0100

    Fix service manager aware

--- index.js
+++ index.js
@@ -2,18 +2,22 @@
  * @type {{Application: Application, Archive: Archive, core: {PaperToastNotification: PaperToastNotification}, EvtManager: EvtManager, hydrator: {HydratorPluginManager: HydratorPluginManager, AbstractHydrator: AbstractHydrator, AggregatePropertyHydrator: AggregatePropertyHydrator, HydratorAware: HydratorAware, PropertyHydrator: PropertyHydrator, strategy: {HydratorStrategy: HydratorStrategy, NumberStrategy: NumberStrategy}}, ModuleConfig: ModuleConfig, net: {P2p: P2p}, sport: {model: {Match: Match, Player: Player, Staff: Staff, Team: Team}}, serviceManager: {ServiceManager: ServiceManager, ServiceManagerAware: ServiceManagerAware}, storage: {StoragePluginManager: StoragePluginManager, Storage: Storage, adapter: {DexieCollection: DexieCollection, LocalStorage: LocalStorage, manager: {DexieManager: DexieManager}}, model: {Dates: Dates}}, Utils: Utils}}
  */
 module.exports = {
-    Application : require('./lib/core/Application.js'),
-    Archive : require('./lib/archive/Archive.js'),
-    core : {
-        PaperToastNotification : require('./lib/core/PaperToastNotification.js'),
-        ModuleConfig : require('./lib/core/ModuleConfig.js')
+    archive : {
+        Archive : require('./lib/archive/Archive.js'),
     },
-    EvtManager : require('./lib/event/EvtManager.js'),
+    core : {
+        Application : require('./lib/core/Application.js'),
+        ModuleConfig : require('./lib/core/ModuleConfig.js'),
+        PaperToastNotification : require('./lib/core/PaperToastNotification.js')
+    },
+    event : {
+        EvtManager : require('./lib/event/EvtManager.js'),
+    },
     hydrator : {
-        HydratorPluginManager : require('./lib/hydrator/HydratorPluginManager.js'),
         AbstractHydrator : require('./lib/hydrator/AbstractHydrator.js'),
         AggregatePropertyHydrator : require('./lib/hydrator/AggregatePropertyHydrator.js'),
         HydratorAware :  require('./lib/hydrator/HydratorAware.js'),
+        HydratorPluginManager : require('./lib/hydrator/HydratorPluginManager.js'),
         PropertyHydrator : require('./lib/hydrator/PropertyHydrator.js'),
         strategy : {
             HydratorStrategy : require('./lib/hydrator/strategy/HydratorStrategy.js'),
@@ -21,11 +25,11 @@ module.exports = {
         }
     },
     net : {
-        P2p : require('./lib/net/p2p/P2p'),
+        P2p : require('./lib/net/p2p/P2p')
     },
     parse : {
-        ObjectToString : require('./lib/parse/ObjectToString'),
         BufferToObject : require('./lib/parse/BufferToObject'),
+        ObjectToString : require('./lib/parse/ObjectToString')
     },
     sport : {
         model : {
@@ -40,8 +44,8 @@ module.exports = {
         ServiceManagerAware : require('./lib/service-manager/ServiceManagerAware')
     },
     storage : {
-        StoragePluginManager : require('./lib/storage/StoragePluginManager.js'),
         Storage : require('./lib/storage/Storage.js'),
+        StoragePluginManager : require('./lib/storage/StoragePluginManager.js'),
         adapter : {
             DexieCollection : require('./lib/storage/indexed-db/dexie/DexieCollection.js'),
             LocalStorage : require('./lib/storage/local-storage/LocalStorage.js'),
@@ -50,7 +54,7 @@ module.exports = {
             }
         },
         model : {
-            Dates : require('./lib/storage/model/Dates'),
+            Dates : require('./lib/storage/model/Dates')
         }
     },
     Utils : require('./lib/Utils.js')
