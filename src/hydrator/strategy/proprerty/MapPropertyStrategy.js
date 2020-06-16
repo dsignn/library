0fbd9a32ff8ec5f1cdd18282f77e5dcf1e52074b
visa4 <antonino.visalli@gmail.com>
2020-06-16 15:11:15 +0200

    Fix propertu map hydrator strategy

---
+++ src/hydrator/strategy/proprerty/MapPropertyStrategy.js
@@ -0,0 +1,34 @@
+/**
+ *
+ */
+export class MapPropertyStrategy {
+    /**
+     * @param {string} hydrate
+     * @param {string} extract
+     */
+    constructor(hydrate, extract) {
+        /**
+         * @type {object}
+         */
+        this.changeExtractProperty = null;
+        /**
+         * @type {null}
+         */
+        this.changeHydrateProperty = null;
+        this.changeHydrateProperty = hydrate;
+        this.changeExtractProperty = extract;
+    }
+    /**
+     * @inheritDoc
+     */
+    extractProperty(property) {
+        return this.changeExtractProperty ? this.changeExtractProperty : property;
+    }
+    /**
+     * @inheritDoc
+     */
+    hydrateProperty(property) {
+        return this.changeHydrateProperty ? this.changeHydrateProperty : property;
+    }
+}
+//# sourceMappingURL=MapPropertyStrategy.js.map
