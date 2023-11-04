c3430550a7cde9d29d95e8bb9d41b99215eb9ee7
visa4 <antonino.visalli@gmail.com>
2023-11-04 12:18:53 +0100

    Fix form data encode

--- src/data-transform/FormDataEncode.js
+++ src/data-transform/FormDataEncode.js
@@ -11,7 +11,26 @@ export class FormDataEncode {
             // skip loop if the property is from prototype
             if (!data.hasOwnProperty(prop))
                 continue;
-            formData.append(prop, data[prop]);
+            switch (true) {
+                case data[prop] === null:
+                    formData.append(prop, data[prop]);
+                    break;
+                case Array.isArray(data[prop]):
+                    // Append array data
+                    for (let cont = 0; data[prop].length > cont; cont++) {
+                        formData.append(`${prop}[]`, data[prop]);
+                    }
+                    break;
+                case typeof data[prop] === 'object' && !(data[prop] instanceof File):
+                    // Append obje t data
+                    for (let intProp in data[prop]) {
+                        formData.append(`${prop}[${intProp}]`, data[prop][intProp]);
+                    }
+                    break;
+                default:
+                    // Append default data
+                    formData.append(prop, data[prop]);
+            }
         }
         return formData;
     }
